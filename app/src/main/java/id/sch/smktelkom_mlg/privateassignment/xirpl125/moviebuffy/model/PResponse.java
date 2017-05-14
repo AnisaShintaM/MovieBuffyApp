937d18f216fae14d4e19b64746fb4b5ae0814c6c
Ocha Putri Perdana Prihatina <ocha_prihatina_24rpl@student.smktelkom-mlg.sch.id>
2017-05-14 21:23:49 +0700

    Retrieving datas from API to fragments

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl125/moviebuffy/model/PResponse.java
@@ -0,0 +1,11 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl125.moviebuffy.model;
+
+import java.util.List;
+
+/**
+ * Created by user on 14/5/2017.
+ */
+
+public class PResponse {
+    public List<Popular> results;
+}
